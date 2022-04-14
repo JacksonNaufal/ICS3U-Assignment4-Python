3bada75a23ceb6914deb87313edcbadd06fd017e
Jackson Naufal <Jackson.naufal@mths.ca>
2022-04-14 19:26:21 +0000

    RectangleOrNotProgram

--- average_number_program.py
+++ average_number_program.py
@@ -2,38 +2,29 @@
 
 # Created by: Jackson Naufal
 # Created on: April 2022
-# This is a average number calculator program
+# This is a is it a square or rectangle program
 
 
 def main():
 
-    # This is a average number calculator
+    # This is a square or rectangle program
 
     # input
-    print("This calculator will calculate your three numbers!")
-    print("Note: this calculator will round to 2 decimal places!")
-    print("Note: Max number is 100, Min number is 0")
-    number_one_string = input("What is your first number?: ")
-    number_two_string = input("What is your second number?: ")
-    number_three_string = input("What is your third number?: ")
+    print("This progrma will tell you if your shape is a square or not!")
+    print("Note: Do not input Letters, 'A', 'B', '...', etc")
+    print("Note: Decimals DO work in this progran")
+    width_string = input("What is the length of your object?: ")
+    length_string = input("What is the width of your object?: ")
     # process & output
     try:
 
-        number_one = float(number_one_string)
-        number_two = float(number_two_string)
-        number_three = float(number_three_string)
-        average = (number_one + number_two + number_three) / 3
-        actual = float(int(average * 100)) / 100
+        width = float(width_string)
+        length = float(length_string)
 
-        if number_one >= 100 or number_one < 0:
-            print("\nInvalid Number, please try again!! (Number 1)")
-        elif number_two >= 100 or number_two < 0:
-            print("\nInvalid Number, please try again!(Number 2)")
-        elif number_three >= 100 or number_three < 0:
-            print("\nInvalid Number, please try again! (Number 3)")
-
+        if width == length:
+            print("\nYour object is a square!")
         else:
-            print("Your average between the 3 numbers is {0}".format(actual))
+            print("\nYour object is a rectangle!")
 
     except Exception:
         print("Invalid Input")
